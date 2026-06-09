89a5372dd32436213cdd82711da776352a311d91
Tanat Kunharee <tanat.kunharee@g.swu.ac.th>
2026-06-09 11:36:47 +0700

    Fix NameError: import generics

--- videos/views.py
+++ videos/views.py
@@ -1,7 +1,7 @@
 import threading
 from django.shortcuts import render
 import os
-from rest_framework import status
+from rest_framework import status,  generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
